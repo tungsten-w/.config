20fa7a6e3a01b2a83897999304932468e13ff8a4
tungsten-w <louis.regis6@orange.fr>
2025-09-01 10:41:51 +0200

    llm+rofi2

--- .scripts/choose-llm.sh
+++ .scripts/choose-llm.sh
@@ -12,16 +12,16 @@ CHOICE=$(echo "$MODELS" | rofi -dmenu -p "LLM" | awk '{print $1}')
 # Lancer Ollama avec le modèle choisi
 case "$CHOICE" in
     󰅒)
-        ghostty --title ollama-popup -e "ollama run mistral" &
+        ghostty -e "ollama run mistral" &
         ;;
     󰾲)
-        ghostty --title ollama-popup -e "ollama run llama3" &
+        ghostty -e "ollama run llama3" &
         ;;
     )
-        ghostty --title ollama-popup -e "ollama run phi3" &
+        ghostty -e "ollama run phi3" &
         ;;
     )
-        ghostty --title ollama-popup -e "ollama run codellama" &
+        ghostty -e "ollama run codellama" &
         ;;
     *)
         notify-send "LLM annulé "
