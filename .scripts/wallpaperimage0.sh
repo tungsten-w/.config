7de1410c671d26b94df1c8a33957e377bed61238
tungsten-w <louis.regis6@orange.fr>
2025-11-24 22:28:49 +0100

    a

--- .scripts/wallpaperimage0.sh
+++ .scripts/wallpaperimage0.sh
@@ -5,7 +5,7 @@ input="/tmp/caca.png"
 output="/tmp/cacae-0.png"
 
 # Redimensionner l'image
-magick "$input" -resize 30% "$output" || { echo "magick a échoué"; exit 1; }
+magick "$input" -resize 40% "$output" || { echo "magick a échoué"; exit 1; }
 
 # Lancer feh détaché (ne bloque plus le script parent)
 # Méthode recommandée : setsid pour créer une nouvelle session, et & pour background
