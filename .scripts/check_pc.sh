4df20bf5e94564fd099fb1d12d3c512eb673393a
tungsten-w <louis.regis6@orange.fr>
2026-01-25 14:04:14 +0100

    broken script

--- .scripts/check_pc.sh
+++ .scripts/check_pc.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
+#!/bin/bash
 
-HOST="100.86.82.121"
+HOST1="100.86.82.121"
+HOST2="100.86.126.26"
 
-if ping -c 1 -W 1 $HOST &>/dev/null; then
-    echo '{"icon":"on","status":"󰣳"}'
+ping1=false
+ping2=false
+
+# Test machine 1
+if ping -c 1 -W 1 "$HOST1" &>/dev/null; then
+    ping1=true
+fi
+
+# Test machine 2
+if ping -c 1 -W 1 "$HOST2" &>/dev/null; then
+    ping2=true
+fi
+
+# Logique d'affichage
+if $ping1 && $ping2; then
+    echo '{"icon":"both","status":"󰒋"}'   # les deux connectées
+elif $ping1; then
+    echo '{"icon":"host1","status":"󰣳"}' # machine 1 seule
+elif $ping2; then
+    echo '{"icon":"host2","status":""}' # machine 2 seule
 else
-    echo '{"icon":"off","status":"󰒲"}'
+    echo '{"icon":"off","status":"󰒲"}'   # aucune
 fi
