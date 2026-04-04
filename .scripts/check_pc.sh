96dd4192f4a32af91618fc85a7ffe97d11f811c2
tungsten-w <louis.regis6@orange.fr>
2026-04-04 16:58:50 +0200

    script check_pc

--- .scripts/check_pc.sh
+++ .scripts/check_pc.sh
@@ -1,29 +1,41 @@
 #!/bin/bash
-#!/bin/bash
 
+#DEFS
 HOST1="100.86.82.121"
-HOST2="100.86.126.26"
+HOST2="100.67.109.40"
+HOST3="100.80.211.22"
 
+#PING
 ping1=false
 ping2=false
+ping3=false
 
-# Test machine 1
+# HOST1
 if ping -c 1 -W 1 "$HOST1" &>/dev/null; then
     ping1=true
 fi
 
-# Test machine 2
+# HOST2
 if ping -c 1 -W 1 "$HOST2" &>/dev/null; then
     ping2=true
 fi
 
-# Logique d'affichage
-if $ping1 && $ping2; then
-    echo '{"icon":"both","status":"󰒍"}'   # les deux connectées
-elif $ping1; then
-    echo '{"icon":"host1","status":"󰣳"}' # machine 1 seule
-elif $ping2; then
-    echo '{"icon":"host2","status":""}' # machine 2 seule
-else
-    echo '{"icon":"off","status":"󰒲"}'   # aucune
+# HOST3
+if ping -c 1 -W 1 "$HOST2" &>/dev/null; then
+    ping3=true
 fi
+
+# Display logic
+if $ping1 && $ping2 && $ping3 ; then
+    echo '{"icon":"both","status":"󰒍"}'   # 3
+elif $ping1 && $ping2; then
+    echo '{"icon":"both","status":"󰒋"}'   # 2
+elif $ping1; then
+    echo '{"icon":"host1","status":"󰣳"}' # 1
+elif $ping2; then
+    echo '{"icon":"host2","status":""}' # 1
+else
+    echo '{"icon":"off","status":"󰒲"}'   # 0
+fi
+
+#replace Host with the ip of your own server ! (0w0)
